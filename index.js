a1acf69deec297604e95d7a69410b108ac89478b
panattog <149946020+panattog@users.noreply.github.com>
2024-03-04 20:44:13 +0000

    Updated files

---
+++ index.js
@@ -0,0 +1,29 @@
+class PandaTea {
+    constructor() {
+      this.teaData = {};
+    }
+  
+    setTeaData(key, value) {
+      this.teaData[key] = value;
+      console.log(`Set Panda Tea data for key '${key}':`, value);
+    }
+  
+    getTeaData(key) {
+      const value = this.teaData[key];
+      console.log(`Retrieved Panda Tea data for key '${key}':`, value);
+      return value;
+    }
+  }
+  
+  // Example usage:
+  const pandaTea = new PandaTea();
+  
+  pandaTea.setTeaData('flavor', 'Bamboo Delight');
+  pandaTea.setTeaData('rating', 4.5);
+  
+  const retrievedFlavor = pandaTea.getTeaData('flavor');
+  const retrievedRating = pandaTea.getTeaData('rating');
+  const retrievedUnknown = pandaTea.getTeaData('unknownKey');
+  
+  console.log('Retrieved Panda Tea data for unknownKey:', retrievedUnknown);
+  
